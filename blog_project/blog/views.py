71ac294461932f39f2e39ccdebe1e4f2879ff192
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-10-09 16:07:09 +0400

    feat(blog): add category filter to post list view and update template with category links

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -3,5 +3,11 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    category = request.GET.get('category')
+
+    if category:
+        posts = Post.objects.filter(category=category)
+    else:
+        posts = Post.objects.all()
+
+    return render(request, 'blog/post_list.html', {'posts': posts, 'selected_category': category})
